63a572311ac81d229c9ac68198acd3cf88656324
Veli Deniz Balcıkoca <vanezx23@gmail.com>
2025-09-09 20:44:32 +0300

    about section fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,10 +2,8 @@ import "../css/About.css"
 
 export default function About(){
     return(
-        <div className="about">
-            <div className="about-center">
-                <p className="about-text">I'm studying Electrical Engineering in Aydın, <span className="red">Türkiye</span>. I like to learn new technologies and concepts. Currently I'm learning <span className="purple">C/C++</span> and <span className="blue">Golang</span> in my free times. I'm mainly interested in game development and electornics.</p>
-            </div>      
+        <div className="about">    
+            <p className="about-text">I'm studying Electrical Engineering in Aydın, <span className="red">Türkiye</span>. I like to learn new technologies and concepts. Currently I'm learning <span className="purple">C/C++</span> and <span className="blue">Golang</span> in my free times. I'm mainly interested in game development and electornics.</p>        
         </div>
     )
 }
