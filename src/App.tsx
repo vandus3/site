d57173d0b564f33be29b5b4d32d234adb2fbc2c9
Veli Deniz Balcıkoca <vanezx23@gmail.com>
2025-09-10 14:40:02 +0300

    repos page skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import "./css/App.css";
 import Lenis from "lenis";
 import Intro from "./components/Intro";
 import About from "./components/About";
+import Repos from "./components/Repos";
+import { use, useEffect, useState } from "react";
+
 export default function App() {
   const lenis = new Lenis({
     autoRaf: true,
@@ -9,10 +12,21 @@ export default function App() {
   lenis.on("scroll", (e) => {
     console.log(e);
   });
+
+  useEffect(() => {
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, [lenis]);
+
   return (
     <div className="app">
       <Intro />
       <About />
+      <Repos />
     </div>
   );
 }
---
+++ src/components/Repos.tsx
@@ -0,0 +1,40 @@
+import "../css/Repos.css";
+import { use, useEffect, useState } from "react";
+type Repo = {
+  id: number;
+  name: string;
+  description: string;
+  html_url: string;
+};
+
+export default function Repos() {
+  const [repos, setRepos] = useState<Repo[]>([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/vandus3/repos")
+      .then((response) => response.json())
+      .then((data) => setRepos(data))
+      .catch((error) => console.error("Error fetching repos:", error));
+  }, []);
+
+  const repoName = repos[0]?.name || "Loading...";
+  const repoDesc = repos[0]?.description || "Loading description...";
+  const repoLink = repos[0]?.html_url || "#";
+  // create a card for every repository
+  return (
+    <div className="repos-container">
+      <h1 className="repos-title">My Repositories</h1>
+      <div className="repos-cards">
+        {repos.map((repo) => (
+          <div className="card" key={repo.id}>
+            <h1 className="card-title">{repo.name}</h1>
+            <p className="card-description">{repo.description}</p>
+            <a href={repo.html_url} className="card-link">
+              View Repository
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
