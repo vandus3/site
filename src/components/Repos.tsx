3489ef1d2201cd625c3cf2c43110f757ee9d8a2b
Veli Deniz Balcıkoca <vanezx23@gmail.com>
2025-09-10 18:09:59 +0300

    polished somethings

--- src/components/Repos.tsx
+++ src/components/Repos.tsx
@@ -20,14 +20,16 @@ export default function Repos() {
   const repoName = repos[0]?.name || "Loading...";
   const repoDesc = repos[0]?.description || "Loading description...";
   const repoLink = repos[0]?.html_url || "#";
-  // create a card for every repository
+  // create a card for every repository yaptık
   return (
     <div className="repos-container">
       <h1 className="repos-title">My Repositories</h1>
       <div className="repos-cards">
         {repos.map((repo) => (
           <div className="card" key={repo.id}>
-            <h1 className="card-title">{repo.name}</h1>
+            <h1 className="card-title">
+              {repo.name.charAt(0).toUpperCase() + repo.name.slice(1)}
+            </h1>
             <p className="card-description">{repo.description}</p>
             <a href={repo.html_url} className="card-link">
               View Repository
